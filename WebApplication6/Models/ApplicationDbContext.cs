a0fe3d482cd601d4cc19d5b069e3836aeacdad3f
my pc <my pc@DESKTOP-SGIKIS6>
2022-02-24 15:21:52 +0530

    Add project files.

---
+++ WebApplication6/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace WebApplication6.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext
+            (DbContextOptions<ApplicationDbContext>options):base(options)
+        { }
+
+        public DbSet<Customer>Customers { get; set; }
+
+        public DbSet<Location> Locations { get; set; }
+    }
+}
